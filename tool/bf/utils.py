c8c122e9a40b78bd321de2e6ac54d0cb0d4654df
Jay <jkrshnmenon@gmail.com>
2022-09-30 11:31:30 -0700

    removing useless dirs

--- tool/bf/utils.py
+++
@@ -1,7 +0,0 @@
-import bf.sinks as sinks
-
-ROUND = 5
-
-SINK_FUNCS = [('strcpy', sinks.strcpy), ('sprintf', sinks.sprintf), ('fwrite', sinks.fwrite), ('memcpy', sinks.memcpy)]
-TIMEOUT_TAINT = 60 * 5
-TIMEOUT_TRIES = 2
